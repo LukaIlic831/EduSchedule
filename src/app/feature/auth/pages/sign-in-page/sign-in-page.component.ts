c92473e723b8865066a2dfa2d1b6619390706cca
LukaIlic831 <luka.ilic3052@gmail.com>
2025-07-05 12:01:33 +0200

    added router link to sign-up-page

--- src/app/feature/auth/pages/sign-in-page/sign-in-page.component.ts
+++ src/app/feature/auth/pages/sign-in-page/sign-in-page.component.ts
@@ -1,17 +1,19 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
-import {MatIconModule } from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-sign-in-page',
-  imports: [ MatCardModule,
+  imports: [
+    MatCardModule,
     MatFormFieldModule,
     MatIconModule,
-    MatInputModule],
+    MatInputModule,
+    RouterLink,
+  ],
   templateUrl: './sign-in-page.component.html',
-  styleUrl: './sign-in-page.component.scss'
+  styleUrl: './sign-in-page.component.scss',
 })
-export class SignInPageComponent {
-
-}
+export class SignInPageComponent {}
