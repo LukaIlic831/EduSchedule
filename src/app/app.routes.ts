c71eea2fbfdab74462ab98e4b948a83f183b5204
LukaIlic831 <luka.ilic3052@gmail.com>
2025-07-05 15:19:14 +0200

    created professor-dashboard-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { SignInPageComponent } from './feature/auth/pages/sign-in-page/sign-in-p
 import { SignUpPageComponent } from './feature/auth/pages/sign-up-page/sign-up-page.component';
 import { ProfessorInfoPageComponent } from './feature/user-info/pages/professor-info-page/professor-info-page.component';
 import { StudentInfoPageComponent } from './feature/user-info/pages/student-info-page/student-info-page.component';
+import { ProfessorDashboardPageComponent } from './feature/dashboard/pages/professor-dashboard-page/professor-dashboard-page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'sign-in', pathMatch: 'full' },
@@ -10,4 +11,5 @@ export const routes: Routes = [
   { path: 'sign-up', component: SignUpPageComponent },
   { path: 'professor-info', component: ProfessorInfoPageComponent },
   { path: 'student-info', component: StudentInfoPageComponent },
+  { path: 'professor-dashboard', component: ProfessorDashboardPageComponent },
 ];
