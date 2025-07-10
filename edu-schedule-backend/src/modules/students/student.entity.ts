3138c7fb642ffafc8e68e7ac4008c4faf99a5750
LukaIlic831 <luka.ilic3052@gmail.com>
2025-07-10 13:29:02 +0200

    created students module

---
+++ edu-schedule-backend/src/modules/students/student.entity.ts
@@ -0,0 +1,32 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+  OneToOne,
+  PrimaryColumn,
+} from 'typeorm';
+import { User } from '../users/user.entity';
+import { StudyProgram } from '../study-programs/study-program.entity';
+import { Seat } from '../seats/seat.entity';
+
+@Entity('students')
+export class Student {
+  @PrimaryColumn()
+  index: number;
+
+  @Column()
+  year: number;
+
+  @OneToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @ManyToOne(() => StudyProgram, (studyProgram) => studyProgram.students)
+  @JoinColumn({ name: 'study_program_id' })
+  studyProgram: StudyProgram;
+
+  @OneToMany(() => Seat, (seat) => seat.student)
+  reservedSeats: Seat[];
+}
