8dc0b1462b05ddd7e6bdea0501bcf87d2ef6404d
LukaIlic831 <luka.ilic3052@gmail.com>
2025-07-10 13:25:14 +0200

    created users module

---
+++ edu-schedule-backend/src/modules/users/user.entity.ts
@@ -0,0 +1,27 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+import { University } from "../universities/university.entity";
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  role: string;
+
+  @Column({ name: 'created_at', type: 'timestamp' })
+  createdAt: Date;
+
+  @ManyToOne(() => University, (university) => university.users)
+  @JoinColumn({ name: 'university_id' })
+  university: University;
+}
