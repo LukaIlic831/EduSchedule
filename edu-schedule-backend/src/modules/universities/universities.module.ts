ecc911eeb45f85db10c2367171a1574c2a7817f2
LukaIlic831 <luka.ilic3052@gmail.com>
2025-07-10 13:27:12 +0200

    created universities module

---
+++ edu-schedule-backend/src/modules/universities/universities.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { UniversitiesController } from './universities.controller';
+import { UniversitiesService } from './universities.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { University } from './university.entity';
+
+@Module({
+  controllers: [UniversitiesController],
+  providers: [UniversitiesService],
+  imports: [TypeOrmModule.forFeature([University])],
+})
+export class UniversitiesModule {}
