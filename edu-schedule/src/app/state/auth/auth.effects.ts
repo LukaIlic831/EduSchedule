6b328fad9a34285df12f67eee667d1c1abc33d7d
LukaIlic831 <luka.ilic3052@gmail.com>
2025-07-11 18:22:12 +0200

    created auth effects

---
+++ edu-schedule/src/app/state/auth/auth.effects.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { signIn, signInFailure, signInSuccess } from './auth.actions';
+import { catchError, map, of, switchMap } from 'rxjs';
+import { AuthService } from '../../core/auth/services/auth.service';
+
+@Injectable()
+export class AuthEffects {
+  constructor(
+    private actions$: Actions,
+    private authService: AuthService
+  ) {}
+  signIn$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(signIn),
+      switchMap(({ email, password }) =>
+        this.authService.signIn({ email, password }).pipe(
+          map((response) => signInSuccess({ token: response.access_token })),
+          catchError((error) => of(signInFailure({ error })))
+        )
+      )
+    )
+  );
+}
