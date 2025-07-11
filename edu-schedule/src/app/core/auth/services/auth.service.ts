ec28e0d420d9103b9d7808af24dbf6a3676c0880
LukaIlic831 <luka.ilic3052@gmail.com>
2025-07-11 20:31:13 +0200

    fixed auth service api route

--- edu-schedule/src/app/core/auth/services/auth.service.ts
+++ edu-schedule/src/app/core/auth/services/auth.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { APIURL } from '../../../data/data';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-  private apiUrl = '/api';
+  private apiUrl = APIURL;
   constructor(private http: HttpClient) {}
 
   signIn(userData: any): Observable<any> {
@@ -14,6 +15,6 @@ export class AuthService {
       email: userData.email,
       password: userData.password,
     };
-    return this.http.post(this.apiUrl + '/auth/login', userDataToSend);
+    return this.http.post(this.apiUrl + '/auth/log-in', userDataToSend);
   }
 }
