cc517da84572d9dcee2e8785ef407ebec6d9190f
LukaIlic831 <luka.ilic3052@gmail.com>
2025-07-11 18:04:00 +0200

    created auth actions

---
+++ edu-schedule/src/app/state/auth/auth.actions.ts
@@ -0,0 +1,16 @@
+import { createAction, props } from '@ngrx/store';
+
+export const signIn = createAction(
+  '[Auth] Sign In',
+  props<{ email: string; password: string }>()
+);
+
+export const signInSuccess = createAction(
+  '[Auth] Sign In Success',
+  props<{ token: string }>()
+);
+
+export const signInFailure = createAction(
+  '[Auth] Sign In Failure',
+  props<{ error: string }>()
+);
