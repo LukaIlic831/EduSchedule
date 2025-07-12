883acc3460352ecff4be805c816c78f51f09a226
LukaIlic831 <luka.ilic3052@gmail.com>
2025-07-12 20:30:41 +0200

    added handleSignOut in professor-dashboard-page

--- edu-schedule/src/app/feature/dashboard/pages/professor-dashboard-page/professor-dashboard-page.component.ts
+++ edu-schedule/src/app/feature/dashboard/pages/professor-dashboard-page/professor-dashboard-page.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { ClassComponent } from '../../../../shared/components/class/class.component';
 import { RouterLink } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { signOut } from '../../../../state/auth/auth.actions';
 
 @Component({
   selector: 'app-professor-dashboard-page',
   imports: [ClassComponent, RouterLink],
   templateUrl: './professor-dashboard-page.component.html',
-  styleUrl: './professor-dashboard-page.component.scss'
+  styleUrl: './professor-dashboard-page.component.scss',
 })
 export class ProfessorDashboardPageComponent {
-
+  constructor(private store: Store) {}
+  handleSignOut() {
+    this.store.dispatch(signOut());
+  }
 }
