091e2ecf960809376fb835166a59da2485d37798
LukaIlic831 <luka.ilic3052@gmail.com>
2025-07-12 20:31:02 +0200

    added handleSignOut in student-profile-page

--- edu-schedule/src/app/feature/profile/pages/student-profile-page/student-profile-page.component.ts
+++ edu-schedule/src/app/feature/profile/pages/student-profile-page/student-profile-page.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { NavComponent } from '../../../../shared/components/nav/nav.component';
 import { ClassComponent } from '../../../../shared/components/class/class.component';
+import { Store } from '@ngrx/store';
+import { signOut } from '../../../../state/auth/auth.actions';
 
 @Component({
   selector: 'app-student-profile-page',
   imports: [NavComponent, ClassComponent],
   templateUrl: './student-profile-page.component.html',
-  styleUrl: './student-profile-page.component.scss'
+  styleUrl: './student-profile-page.component.scss',
 })
 export class StudentProfilePageComponent {
-
+  constructor(private store: Store) {}
+  handleSignOut() {
+    this.store.dispatch(signOut());
+  }
 }
