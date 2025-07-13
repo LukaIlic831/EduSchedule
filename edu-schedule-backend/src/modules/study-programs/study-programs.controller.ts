6edb9b41da1edf913951fa8ec17c41d03559506c
LukaIlic831 <luka.ilic3052@gmail.com>
2025-07-13 23:49:27 +0200

    created studyProgramsByUniversityId route

--- edu-schedule-backend/src/modules/study-programs/study-programs.controller.ts
+++ edu-schedule-backend/src/modules/study-programs/study-programs.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { StudyProgramsService } from './study-programs.service';
+import { StudyProgram } from './study-program.entity';
 
 @Controller('study-programs')
-export class StudyProgramsController {}
+export class StudyProgramsController {
+  constructor(private readonly studyProgramsService: StudyProgramsService) {}
+
+  @Get('university/')
+  getStudyProgramsByUniversityId(
+    @Query('universityId') universityId: number,
+  ): Promise<StudyProgram[]> {
+    return this.studyProgramsService.getStudyProgramsByUniversityId(
+      universityId,
+    );
+  }
+}
--- edu-schedule-backend/src/modules/study-programs/study-programs.service.ts
+++ edu-schedule-backend/src/modules/study-programs/study-programs.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { StudyProgram } from './study-program.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class StudyProgramsService {}
+export class StudyProgramsService {
+  constructor(
+    @InjectRepository(StudyProgram)
+    private readonly studyProgramRepository: Repository<StudyProgram>,
+  ) {}
+
+  getStudyProgramsByUniversityId(
+    universityId: number,
+  ): Promise<StudyProgram[]> {
+    return this.studyProgramRepository.find({
+      where: {
+        university: {
+          id: universityId,
+        },
+      },
+    });
+  }
+}
