abe64015281b7d1bd9bd15a19b656a40814f54c8
LukaIlic831 <luka.ilic3052@gmail.com>
2025-07-14 14:26:18 +0200

    display professor data

--- edu-schedule/src/app/feature/dashboard/pages/professor-dashboard-page/professor-dashboard-page.component.ts
+++ edu-schedule/src/app/feature/dashboard/pages/professor-dashboard-page/professor-dashboard-page.component.ts
@@ -3,15 +3,21 @@ import { ClassComponent } from '../../../../shared/components/class/class.compon
 import { RouterLink } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { signOut } from '../../../../state/auth/auth.actions';
+import { selectAuthUser } from '../../../../state/auth/auth.selectors';
+import { User } from '../../../../state/auth/models/user.model';
+import { Observable, of } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-professor-dashboard-page',
-  imports: [ClassComponent, RouterLink],
+  imports: [ClassComponent, RouterLink, CommonModule],
   templateUrl: './professor-dashboard-page.component.html',
   styleUrl: './professor-dashboard-page.component.scss',
 })
 export class ProfessorDashboardPageComponent {
+  user: Observable<User | null> = of(null);
   constructor(private store: Store) {
+    this.user = this.store.select(selectAuthUser);
   }
   handleSignOut() {
     this.store.dispatch(signOut());
