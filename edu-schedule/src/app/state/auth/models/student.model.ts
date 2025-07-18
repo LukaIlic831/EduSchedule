206c0e1ee8ec6860590b2357f299472a8edc8625
LukaIlic831 <luka.ilic3052@gmail.com>
2025-07-18 18:19:18 +0200

    updated student model

--- edu-schedule/src/app/state/auth/models/student.model.ts
+++ edu-schedule/src/app/state/auth/models/student.model.ts
@@ -1,6 +1,7 @@
 import { StudyProgram } from "../../education-data/models/study-program.model";
 
 export interface Student {
+    userId:number;
     index: number;
     year: number;
     studyProgram: StudyProgram;
