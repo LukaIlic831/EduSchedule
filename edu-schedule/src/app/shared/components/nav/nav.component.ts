88f85be67e69e58f6bed90f485ccc3c82e42c161
LukaIlic831 <luka.ilic3052@gmail.com>
2025-07-18 18:24:59 +0200

    added search route in nav

--- edu-schedule/src/app/shared/components/nav/nav.component.ts
+++ edu-schedule/src/app/shared/components/nav/nav.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [MatIconModule, RouterLink],
+  imports: [MatIconModule, RouterLink, CommonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
