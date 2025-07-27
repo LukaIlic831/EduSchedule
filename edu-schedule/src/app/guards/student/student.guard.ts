4549eea522ceb4ee33de4ccdcb9ad88e11223f7e
LukaIlic831 <luka.ilic3052@gmail.com>
2025-07-27 14:39:38 +0200

    created student guard

---
+++ edu-schedule/src/app/guards/student/student.guard.ts
@@ -0,0 +1,38 @@
+import { inject } from '@angular/core';
+import { CanActivateFn, Router, UrlTree } from '@angular/router';
+import { Store } from '@ngrx/store';
+import {
+  selectAuthLoading,
+  selectAuthUserProfessor,
+  selectAuthUserStudent,
+} from '../../state/auth/auth.selectors';
+import { combineLatest, filter, Observable, of, switchMap, take } from 'rxjs';
+
+export const studentGuard: CanActivateFn = (
+  route,
+  state
+): Observable<boolean | UrlTree> => {
+  const store = inject(Store);
+  const router = inject(Router);
+  return store.select(selectAuthLoading).pipe(
+    filter((loading) => !loading),
+    take(1),
+    switchMap(() =>
+      combineLatest([
+        store.select(selectAuthUserProfessor),
+        store.select(selectAuthUserStudent),
+      ]).pipe(
+        take(1),
+        switchMap(([professor, student]) => {
+          if (professor) {
+            return of(router.createUrlTree(['/professor-dashboard']));
+          }
+          if (!student) {
+            return of(router.createUrlTree(['/student-info']));
+          }
+          return of(true);
+        })
+      )
+    )
+  );
+};
